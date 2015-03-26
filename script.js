56f162b4fd6d7c29d3666a8f7c687db37ce97ea3
NerdyItalian <NerdyItalian@users.noreply.github.com>
2015-03-26 10:51:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function sumOfSquare(a, b) {
-	return a*a + b*b;
+var theNumber = 1;
+ 
+var michelangelo = function(value){
+    value = (value * 2);
+    return value;
 }
-	console.log(sumOfSquare(4,6))
+ 
+var donatello = function(value, anotherValue){
+    value = anotherValue + value;
+    anotherValue = value * anotherValue;
+    return anotherValue;
+}
+ 
+var leonardo = function(value){
+    var anotherValue = value;
+    value *= 4;
+    theNumber = value / 2;
+    return (value + anotherValue);
+}
+ 
+var raphael = function(value, anotherValue, yetAnotherValue){
+    value = value * anotherValue + yetAnotherValue;
+    return yetAnotherValue;
+}
+ 
